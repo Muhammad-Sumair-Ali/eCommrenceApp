b4f8cb37f2d0d13f79453f6787fd1ab06bc8d07e
Muhammad-Sumair-Ali <sumairjattoi@gmail.com>
2024-09-29 17:29:37 +0500

    new

--- src/index.jsx
+++ src/index.jsx
@@ -1,28 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './App.css'
-import App from './App'
-import 'swiper/css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./App.css";
+import App from "./App";
+import "swiper/css";
 import AuthProvider from "./App/context/AuthContext";
-import { Toaster } from 'react-hot-toast';
+import { Toaster } from "react-hot-toast";
 import {
   useQuery,
   useMutation,
   useQueryClient,
   QueryClient,
   QueryClientProvider,
-} from '@tanstack/react-query'
+} from "@tanstack/react-query";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-   <>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
     <AuthProvider>
       <QueryClientProvider client={queryClient}>
-		 <App />
-      <Toaster />
+        <App />
+        <Toaster />
       </QueryClientProvider>
     </AuthProvider>
-   </>
-
-)
+  </>
+);
